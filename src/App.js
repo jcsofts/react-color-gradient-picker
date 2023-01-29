a81e03bc01af3e7dc6a65fc40531760c498e9456
fedora <fedors@flowtrack.co>
2023-01-29 10:04:02 +0800

    cursor pointer

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     };
 
     const button=(
-        <div style={{ position: 'absolute',top:-8,right:-8,zIndex:"1" }}><i className="far fa-times-circle"></i></div>
+        <div style={{ position: 'absolute',top:-8,right:-8,zIndex:"1" }}><i className="far fa-times-circle" style={{ cursor:"pointer" }}></i></div>
     );
 
     return (
